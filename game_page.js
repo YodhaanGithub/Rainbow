4e24fb666ad4b9934cea0ae88c64fa74b5e2c312
YodhaanGithub <76775863+YodhaanGithub@users.noreply.github.com>
2021-07-31 13:19:51 +0530

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD7OmGHTPFvfRmtBu6WBudZKB3KFQp-Hac",
+    authDomain: "yitter-2d72b.firebaseapp.com",
+    projectId: "yitter-2d72b",
+    storageBucket: "yitter-2d72b.appspot.com",
+    messagingSenderId: "709457651413",
+    appId: "1:709457651413:web:4608924bb69ab127bde45c"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
